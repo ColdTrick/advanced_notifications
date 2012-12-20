9b8b1ec3bb3a1b66315efc9145dddef81e654f13
Jeroen Dalsem <jdalsem@coldtrick.com>
2012-12-20 10:54:41 +0100

    added a dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,11 @@
+<?php
+$language = array (
+  'advanced_notifications:cli:error:secret' => 'Opgegeven secret is niet geldig',
+  'advanced_notifications:cli:error:type' => 'Opgegeven type is niet geldig',
+  'advanced_notifications:settings:replace_site_notifications' => 'Wilt u de systeem notificaties, die normaal gesproken in de inbox verschijnen, vervangen door een gepersonaliseerde activiteiten pagina (gebaseerd op de notificatie instellingen)?',
+  'advanced_notifications:activity:groups' => 'Mijn groepen',
+  'advanced_notifications:activity:groups:info' => 'Activiteit in groepen waar ik lid van ben',
+  'advanced_notifications:activity:notifications' => 'Gepersonaliseerd',
+  'advanced_notifications:activity:notifications:info' => 'Activiteit gebaseerd op mijn notificatie instellingen',
+);
+add_translation("nl", $language);
