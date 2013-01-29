c565756b31fc921361d4bd1971e47d6404fcb2fa
Jerome Bakker <jeabakker@coldtrick.com>
2013-01-29 14:12:14 +0100

    changed: moved plugin hook functions to own file

---
+++ lib/hooks.php
@@ -0,0 +1,54 @@
+<?php
+
+	function advanced_notifications_route_hook($hooks, $type, $return_value, $params){
+		$page = $return_value["segments"];
+	
+		if(!empty($page)){
+				
+			switch($page[0]) {
+				case "notifications":
+				case "groups":
+					if(elgg_is_logged_in()){
+						elgg_set_page_owner_guid(elgg_get_logged_in_user_guid());
+						set_input('page_type', $page[0]);
+	
+						require_once(elgg_get_plugins_path() . "advanced_notifications/pages/river.php");
+						return false;
+					} else {
+						forward("activity");
+					}
+						
+			}
+		}
+	}
+	
+	function advanced_notifications_filter_menu_hook($hooks, $type, $return_value, $params){
+		$result = $return_value;
+	
+		if(elgg_is_logged_in() && elgg_in_context("activity")){
+			// add new items
+			if(elgg_is_active_plugin("groups")){
+				$result[] = ElggMenuItem::factory(array(
+							'text' => elgg_echo('advanced_notifications:activity:groups'),
+							'href' => 'activity/groups',
+							'name' => 'groups',
+							'contexts' => array("activity"),
+							'priority' => 900,
+							'title' => elgg_echo('advanced_notifications:activity:groups:info')
+				));
+			}
+				
+			if(elgg_is_active_plugin("messages")){
+				$result[] = ElggMenuItem::factory(array(
+							'text' => elgg_echo('advanced_notifications:activity:notifications'),
+							'href' => 'activity/notifications',
+							'name' => 'notifications',
+							'contexts' => array("activity"),
+							'priority' => 910,
+							'title' => elgg_echo('advanced_notifications:activity:notifications:info')
+				));
+			}
+		}
+	
+		return $result;
+	}
--- start.php
+++ start.php
@@ -1,7 +1,8 @@
 <?php
 
-	require_once(dirname(__FILE__) . "/lib/functions.php");
 	require_once(dirname(__FILE__) . "/lib/events.php");
+	require_once(dirname(__FILE__) . "/lib/functions.php");
+	require_once(dirname(__FILE__) . "/lib/hooks.php");
 	
 	elgg_register_event_handler("init", "system", "advanced_notifications_init");
 	
@@ -26,55 +27,3 @@
 		}		
 	}
 	
-	function advanced_notifications_route_hook($hooks, $type, $return_value, $params){
-		$page = $return_value["segments"];
-		
-		if(!empty($page)){
-			
-			switch($page[0]) {
-				case "notifications":
-				case "groups":
-					if(elgg_is_logged_in()){
-						elgg_set_page_owner_guid(elgg_get_logged_in_user_guid());
-						set_input('page_type', $page[0]);
-						
-						require_once(elgg_get_plugins_path() . "advanced_notifications/pages/river.php");
-						return false;
-					} else {
-						forward("activity");
-					}
-					
-			}
-		}		
-	}
-	
-	function advanced_notifications_filter_menu_hook($hooks, $type, $return_value, $params){
-		$result = $return_value;
-		
-		if(elgg_is_logged_in() && elgg_in_context("activity")){
-			// add new items
-			if(elgg_is_active_plugin("groups")){
-				$result[] = ElggMenuItem::factory(array(
-						'text' => elgg_echo('advanced_notifications:activity:groups'),
-						'href' => 'activity/groups',
-						'name' => 'groups',
-						'contexts' => array("activity"),
-						'priority' => 900,
-						'title' => elgg_echo('advanced_notifications:activity:groups:info')
-				));
-			}
-			
-			if(elgg_is_active_plugin("messages")){
-				$result[] = ElggMenuItem::factory(array(
-						'text' => elgg_echo('advanced_notifications:activity:notifications'),
-						'href' => 'activity/notifications',
-						'name' => 'notifications',
-						'contexts' => array("activity"),
-						'priority' => 910,
-						'title' => elgg_echo('advanced_notifications:activity:notifications:info')
-				));
-			}
-		}
-		
-		return $result;
-	}
