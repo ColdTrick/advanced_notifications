38fffaa8e6dccff684e74cea40f5c8b5914b74f2
Jerome Bakker <jeabakker@coldtrick.com>
2013-10-16 15:09:52 +0200

    updated Dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -1,5 +1,8 @@
 <?php
 $language = array (
+  'advanced_notifications:settings:no_mail_content' => 'Wil je de content notificatie berichten vervangen met alleen een link naar de content?',
+  'advanced_notifications:notification:email:body' => 'Om de content te bekijken, klik hier
+%s',
   'advanced_notifications:cli:error:secret' => 'Opgegeven secret is niet geldig',
   'advanced_notifications:cli:error:type' => 'Opgegeven type is niet geldig',
   'advanced_notifications:settings:replace_site_notifications' => 'Wilt u de systeem notificaties, die normaal gesproken in de inbox verschijnen, vervangen door een gepersonaliseerde activiteiten pagina (gebaseerd op de notificatie instellingen)?',
@@ -9,6 +12,6 @@ $language = array (
   'advanced_notifications:activity:notifications' => 'Gepersonaliseerd',
   'advanced_notifications:activity:notifications:title' => 'Gepersonaliseerde activiteiten',
   'advanced_notifications:activity:notifications:info' => 'Activiteit gebaseerd op mijn notificatie instellingen',
-  'advanced_notifications:discussion:reply:subject' => "Er is gereageerd op een discussie",
+  'advanced_notifications:discussion:reply:subject' => 'Er is gereageerd op een discussie',
 );
 add_translation("nl", $language);
