53c104cc7d76467c3bc6a610da21eff01d7778c8
Jerome Bakker <jeabakker@coldtrick.com>
2018-12-14 14:44:12 +0100

    chore: updated Dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -1,10 +1,13 @@
 <?php
 /**
- * This file was created by Translation Editor v6.0
- * On 2018-02-27 16:35
+ * This file was created by Translation Editor v7.0-beta
+ * On 2018-12-14 14:43
  */
 
 return array (
+  'advanced_notifications:settings:owner' => 'Eigenaar notificatie instellingen',
+  'advanced_notifications:settings:owner_subscriptions:description' => 'Hieronder staat een lijst met content types waarvoor notificaties worden verstuurd.
+Indien voor een content type de abonnees niet moeten worden uitgebreid met de abonnees van de eigenaar, kun je dat uitschakelen.',
   'advanced_notifications:settings:queue_delay' => 'Notificatie wachtrij vertraging (seconden)',
   'advanced_notifications:settings:queue_delay:help' => 'Items zullen uit de wachtrij worden opgepakt na de opgegeven vertraging',
   'advanced_notifications:settings:notify_owner_subscribers' => 'Verstuur een notificatie naar de abonnees van de eigenaar over nieuwe content',
